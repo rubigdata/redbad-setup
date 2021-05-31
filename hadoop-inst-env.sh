d90a4fc04a8f63fa35b621fb5c09d037b063c84d
Arjen P. de Vries <arjen@acm.org>
2021-05-31 16:40:02 +0000

    Create /var/data directory.

--- hadoop-inst-env.sh
+++ hadoop-inst-env.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-: ${1?"Usage: $0 password-prefix"}
+: ${1?"Usage: sudo $0 password-prefix"}
 PASS=$1
-HADOOP_GID=2021
-HADOOP_UID=2021
+HADOOP_GID=321
+HADOOP_UID=321
 MAPRED_UID=$((HADOOP_UID+1))
 SPARK_UID=$((HADOOP_UID+2))
 ZK_UID=$((HADOOP_UID+3))
 groupadd -r -g ${HADOOP_GID} hadoop
-useradd -r -m -u ${HADOOP_UID} -g ${HADOOP_GID} -d /opt/hadoop hdfs
-useradd -r -m -u ${MAPRED_UID} -g ${HADOOP_GID} -d /opt/mapred mapred
-useradd -r -m -u ${SPARK_UID} -g ${HADOOP_GID} -d /opt/spark spark
-useradd -r -m -u ${ZK_UID} -g ${HADOOP_GID} -d /opt/zookeeper zookeeper
+useradd -r -M -u ${HADOOP_UID} -g ${HADOOP_GID} -d /opt/hadoop hdfs
+useradd -r -M -u ${MAPRED_UID} -g ${HADOOP_GID} -d /opt/mapred mapred
+useradd -r -M -u ${SPARK_UID} -g ${HADOOP_GID} -d /opt/spark spark
+useradd -r -M -u ${ZK_UID} -g ${HADOOP_GID} -d /opt/zookeeper zookeeper
 echo ${PASS}-h@d00p | passwd --stdin hdfs
 echo ${PASS}-m@pr3d | passwd --stdin mapred
 echo ${PASS}-sp@rk | passwd --stdin spark
@@ -19,12 +19,4 @@ usermod -aG wheel,sudo hdfs
 usermod -aG wheel,sudo mapred
 usermod -aG wheel,sudo spark
 usermod -aG wheel,sudo zookeeper
-# Data directory
-sudo mkdir -p /var/data
-# Zookeeper data directory and symlink
-[ -d /var/storage/data8 ] && ZKDATA=/var/storage/data8/zookeeper \
-                          || ZKDATA=/var/storage/data2/zookeeper
-sudo mkdir -p $ZKDATA
-sudo chown -R zookeeper:hadoop $ZKDATA
-sudo ln -s $ZKDATA /var/data/zookeeper
-sudo chown -R zookeeper:hadoop /var/data/zookeeper
+mkdir -p /var/data
