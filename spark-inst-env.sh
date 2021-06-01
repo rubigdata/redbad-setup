4a18bdf9c45b5452b4e6f1528df7654527b41168
Arjen P. de Vries <arjen@acm.org>
2021-06-01 12:14:43 +0000

    Copy over spark-conf files.

--- spark-inst-env.sh
+++ spark-inst-env.sh
@@ -6,6 +6,7 @@ sudo mv /opt/spark-3.1.1-bin-without-hadoop /opt/spark
 sudo chown -R spark:hadoop /opt/spark
 #
 cp spark.rc $HOME/.bashrc.d/spark.rc
+sudo -u spark cp spark-conf/* /opt/spark/conf
 #
 # SSH
 sudo -u spark mkdir -p /opt/spark/.ssh/authorized_keys.d
