c61363c3c3d0e5cbb16cd08ee62dbc48cf8b6133
Arjen P. de Vries <arjen@acm.org>
2021-06-02 00:55:03 +0000

    Untested update

--- hadoop-mk-dn-dir.sh
+++ hadoop-mk-dn-dir.sh
@@ -4,20 +4,26 @@
 # Table 4.4 from Eric Sammer's Hadoop Operations (2012).
 # Updates: hadoop-project-dist/hadoop-common/DeprecatedProperties.html
 #
-for n in 1 2 3 4 5 6
+for c in gelre frisia
 do
-    #
-    # dfs.datanode.data.dir
-    d="/var/storage/data${n}/dfs/dn"
-    mkdir -p $d
-    chown hdfs:hadoop $d
-    chmod 0700 $d
-    #
-    # mapreduce.job.local.dir
-    d="/var/storage/data${n}/mapred/local"
-    mkdir -p $d
-    chown mapred:hadoop $d
-    chmod 0770 $d    
+    for n in 1 2 3 4 5 6
+    do
+        d=/var/storage/data${n}/dfs
+        sudo mkdir -p $d
+        sudo chown -R hdfs:hadoop $d
+        #
+        # dfs.datanode.data.dir
+        d="/var/storage/data${n}/dfs/$c/dn"
+        mkdir -p $d
+        chown hdfs:hadoop $d
+        chmod 0700 $d
+        #
+        # mapreduce.job.local.dir
+        d="/var/storage/data${n}/mapred/$c/local"
+        mkdir -p $d
+        chown mapred:hadoop $d
+        chmod 0770 $d    
+    done
 done
 #
 # mapreduce.jobtracker.system.dir (1/fed)
@@ -34,18 +40,17 @@ for c in gelre frisia
 do
     #
     # $HADOOP_LOG_DIR
-    d=/var/log/hadoop	
-    mkdir -p $d
-    chown root:hadoop $d
-    chmod 0775 $d
+    d=/var/log/hadoop/${c}	
+    sudo mkdir -p $d
+    sudo chown root:hadoop $d
+    sudo chmod 0775 $d
     #
     # mapreduce.cluster.temp.dir
     d=/var/data1/tmp/hadoop-${c}-hdfs
-    mkdir -p $d
-    chown root:root $d
-    chmod 1777 $d
+    sudo mkdir -p $d
+    sudo chown root:root $d
+    sudo chmod 1777 $d
 done
-
 # TODO: (?)
 # mapreduce.cluster.local.dir 
 # mapreduce.cluster.temp.dir
