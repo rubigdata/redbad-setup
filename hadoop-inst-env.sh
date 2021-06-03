2c568f8766011898470a1418831bfe41ae51f13d
Arjen P. de Vries <arjen@acm.org>
2021-06-03 22:17:08 +0000

    Make sure hadoop.rc and spark.rc are installed.

--- hadoop-inst-env.sh
+++ hadoop-inst-env.sh
@@ -6,4 +6,6 @@ sudo mv /opt/hadoop-3.2.2 /opt/hadoop
 sudo chown -R hdfs:hadoop /opt/hadoop
 #
 cp hadoop.rc $HOME/.bashrc.d/hadoop.rc
-# sudo -u hdfs cp hadoop-conf/* /opt/hadoop/etc/hadoop/
+sudo -u hdfs mkdir -p /opt/hadoop/.bashrc.d
+sudo -u hdfs cp hadoop.rc /opt/hadoop/.bashrc.d/hadoop.rc
+sudo -u hdfs cp hadoop-conf/* /opt/hadoop/etc/hadoop/
--- spark-inst-env.sh
+++ spark-inst-env.sh
@@ -5,5 +5,6 @@ sudo tar -xf /home/core/dist/spark-3.1.1-bin-without-hadoop.tgz -C /opt/
 sudo mv /opt/spark-3.1.1-bin-without-hadoop /opt/spark
 sudo chown -R spark:hadoop /opt/spark
 #
-cp spark.rc $HOME/.bashrc.d/spark.rc
+sudo -u spark mkdir -p /opt/spark/.bashrc.d
+sudo -u spark cp spark.rc /opt/spark/.bashrc.d/spark.rc
 sudo -u spark cp spark-conf/* /opt/spark/conf
