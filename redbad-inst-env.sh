abf3e485e4296804751d776286b808deb41c19af
Arjen P. de Vries <arjen@acm.org>
2021-06-04 01:55:37 +0000

    Works better if we create homedir.

--- redbad-inst-env.sh
+++ redbad-inst-env.sh
@@ -9,11 +9,11 @@ SPARK_UID=$((HADOOP_UID+2))
 ZK_UID=$((HADOOP_UID+3))
 YARN_UID=$((HADOOP_UID+4))
 groupadd -r -g ${HADOOP_GID} hadoop
-useradd -r -M -u ${HADOOP_UID} -g ${HADOOP_GID} -d /opt/hadoop hdfs
-useradd -r -M -u ${MAPRED_UID} -g ${HADOOP_GID} -d /opt/mapred mapred
-useradd -r -M -u ${SPARK_UID} -g ${HADOOP_GID} -d /opt/spark spark
-useradd -r -M -u ${ZK_UID} -g ${HADOOP_GID} -d /opt/zookeeper zookeeper
-useradd -r -M -u ${YARN_UID} -g ${HADOOP_GID} -d /opt/yarn yarn
+useradd -r -m -u ${HADOOP_UID} -g ${HADOOP_GID} -d /opt/hadoop hdfs
+useradd -r -m -u ${MAPRED_UID} -g ${HADOOP_GID} -d /opt/mapred mapred
+useradd -r -m -u ${SPARK_UID} -g ${HADOOP_GID} -d /opt/spark spark
+useradd -r -m -u ${ZK_UID} -g ${HADOOP_GID} -d /opt/zookeeper zookeeper
+useradd -r -m -u ${YARN_UID} -g ${HADOOP_GID} -d /opt/yarn yarn
 echo ${PASS}-h@d00p | passwd --stdin hdfs
 echo ${PASS}-m@pr3d | passwd --stdin mapred
 echo ${PASS}-sp@rk | passwd --stdin spark
