765c2140a98920e78b0bba18d269c330ad8c9c5e
Arjen P. de Vries <arjen@acm.org>
2021-06-04 00:26:20 +0000

    Fixes

---
+++ hadoop-mk-yarn-dir.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Directories to be created on nodes
+# Table 4.4 from Eric Sammer's Hadoop Operations (2012).
+# Updates: hadoop-project-dist/hadoop-common/DeprecatedProperties.html
+#
+for c in gelre frisia
+do
+    n=1
+    d=/var/storage/data${n}/yarn
+    sudo mkdir -p $d
+    sudo chown -R root:yarn $d
+    #
+    # yarn.nodemanager.local-dirs
+    d="/var/storage/data${n}/yarn/$c/local"
+    sudo mkdir -p $d
+    sudo chown root:yarn $d
+    sudo chmod 0700 $d
+    #
+    # yarn.nodemanager.log-dirs
+    d="/var/storage/data${n}/yarn/$c/logs"
+    sudo mkdir -p $d
+    sudo chown root:yarn $d
+    sudo chmod 0770 $d    
+done
