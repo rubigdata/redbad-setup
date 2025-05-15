8c76e8fc0467d64796d058525053e7bcf3341363
Arjen P. de Vries <arjen@acm.org>
2025-05-15 18:29:45 +0200

    Java is layered into OS, not needed in toolbox (I think)

--- toolbox-inst.sh
+++ toolbox-inst.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 toolbox create -y
-toolbox run sudo dnf -y install java-21-openjdk-devel
 toolbox run sudo wget --quiet --show-progress --progress=bar:force -O /etc/yum.repos.d/sbt-rpm.repo https://www.scala-sbt.org/sbt-rpm.repo
 toolbox run sudo dnf -y install sbt
