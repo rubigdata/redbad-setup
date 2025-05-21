9982783c77bf8d4e299e34c1f0196a6dd9444247
Arjen P. de Vries <arjen@acm.org>
2025-05-21 22:09:21 +0200

    Comment to make clear you may not need this any longer

--- hadoop-inst-env-2.sh
+++ hadoop-inst-env-2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# NB: this is now integrated in hadoop-inst-env.sh, you probably do not need it
+#
 [ ! -f /opt/hadoop/.bashrc ] && sudo -u hdfs cp /etc/skel/.bash* /opt/hadoop
 sudo -u hdfs mkdir -p /opt/hadoop/.bashrc.d
 sudo -u hdfs cp hadoop.rc /opt/hadoop/.bashrc.d
