c33b82e0a3e41803fd9aeadb4f3532a6e5332499
Arjen P. de Vries <arjen@acm.org>
2025-05-21 17:12:19 +0200

    Add commands to source correct bashrc files when user is hdfs

--- hadoop-inst-env.sh
+++ hadoop-inst-env.sh
@@ -12,6 +12,10 @@ sudo -u hdfs mkdir -p /opt/hadoop/.bashrc.d
 sudo -u hdfs cp hadoop.rc /opt/hadoop/.bashrc.d/hadoop.rc
 sudo -u hdfs cp hadoop-conf/* /opt/hadoop/etc/hadoop/
 #
+[ ! -f /opt/hadoop/.bashrc ] && sudo -u hdfs cp /etc/skel/.bash* /opt/hadoop
+sudo -u hdfs mkdir -p /opt/hadoop/.bashrc.d
+sudo -u hdfs cp hadoop.rc /opt/hadoop/.bashrc.d
+#
 sudo chown root:hadoop /opt/hadoop/bin/container-executor
 sudo chmod 050 /opt/hadoop/bin/container-executor
 sudo chmod u+s /opt/hadoop/bin/container-executor
