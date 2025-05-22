073c81028a0faa1a0f8b6e6c5955f22d0d312a1a
Gijs Hendriksen <info@gijshendriksen.nl>
2025-05-22 08:54:37 +0200

    Replace mv by ln

--- spark-inst-env.sh
+++ spark-inst-env.sh
@@ -4,8 +4,10 @@
 . ./versions.rc
 #
 sudo tar -xf /home/core/dist/spark-${SPARK_VER}-bin-without-hadoop.tgz -C /opt/
-sudo mv /opt/spark-${SPARK_VER}-bin-without-hadoop /opt/spark
-sudo chown -R spark:hadoop /opt/spark
+sudo mv /opt/spark-${SPARK_VER}-bin-without-hadoop /opt/spark-${SPARK_VER}
+sudo ln -sf /opt/spark-${SPARK_VER} /opt/spark
+sudo chown spark:hadoop /opt/spark
+sudo chown -R spark:hadoop /opt/spark-${SPARK_VER}
 #
 sudo -u spark mkdir -p /opt/spark/.bashrc.d
 sudo -u spark cp spark.rc /opt/spark/.bashrc.d/spark.rc
